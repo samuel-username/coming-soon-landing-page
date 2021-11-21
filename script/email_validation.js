e560d2bc50a4879ffaf3991a823753e8b5c0aae7
samuel-username <sadepoju939@gmail.com>
2021-11-21 09:32:59 +0100

    added files

---
+++ script/email_validation.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#email");
+const errorOutput = document.querySelector(".errMsg");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const formData = new FormData(e.target).entries();
+  const { email } = Object.fromEntries(formData);
+  const errorText = validateEmail(email);
+  if (errorText !== "valid") {
+    errorOutput.innerText = errorText;
+  } else {
+    e.target.submit();
+  }
+})
+
+input.addEventListener("input", e => {
+  errorOutput.innerText = "";
+})
+
+function validateEmail(email) {
+  if (!email) return 'Email is required';
+    
+  const isValidEmail = /^\S+@\S+\.\S+$/g;
+  if (!isValidEmail.test(email)) {
+    return 'Please provide a valid email';
+  }
+  return 'valid';
+}
